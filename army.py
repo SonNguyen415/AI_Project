fdd02f45c551252d238212c68af4cf4ba287a113
Son Nguyen <Son.mofa415@gmail.com>
2024-11-20 16:07:35 -0500

    Skeleton class for the army

--- army.py
+++ army.py
@@ -1,3 +1,21 @@
 """
 This file will contain logic for the army system. This includes movement  
 """
+
+class Army:
+    def __init__(self, size):
+        self.size = 0
+        self.owner = 0
+        self.power = 0
+
+    def get_legal_moves(self):
+        """
+        Return all the possible legal moves the army can go from this position
+        """
+        raise NotImplementedError
+    
+    def move(self):
+        """
+        Move the army
+        """
+        raise NotImplementedError
